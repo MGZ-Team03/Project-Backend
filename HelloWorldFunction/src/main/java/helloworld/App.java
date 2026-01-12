c74b8f16082759c5a53297c6acc794d14f59714f
waney <ssdii66@gmail.com>
2026-01-12 16:20:08 +0900

    cd test

--- HelloWorldFunction/src/main/java/helloworld/App.java
+++ HelloWorldFunction/src/main/java/helloworld/App.java
@@ -33,7 +33,7 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
                 .withHeaders(headers);
         try {
 
-            responseBody.put("message", "Hello World cd 테스트!!!!");
+            responseBody.put("message", "Hello World cd 테스트###");
             responseBody.put("cd", "cd 테스트!!");
 
             return response
