1af35cc2b680885313a69443e3bc16d84fc961bf
waney <ssdii66@gmail.com>
2026-01-21 17:00:37 +0900

    [#23] 1분에 한번식 데이터 전송 + 접속하면 바로 데이터 전송

--- SentencesWebSocketFunction/src/main/java/websocket/SocketHandler.java
+++ SentencesWebSocketFunction/src/main/java/websocket/SocketHandler.java
@@ -5,10 +5,12 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2WebSocketEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2WebSocketResponse;
 import com.google.gson.Gson;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import websocket.config.DynamoDbConfig;
 import websocket.controller.SocketController;
 import websocket.repository.SocketRepository;
 import websocket.service.SocketService;
+import websocket.utils.StudentStatusCollector;
 
 /**
  * Handler for requests to Lambda function.
@@ -22,12 +24,23 @@ public class SocketHandler implements RequestHandler<APIGatewayV2WebSocketEvent,
         // 환경 변수에서 테이블 이름 가져오기
         String tableName = System.getenv("TUTOR_STUDENTS_TABLE");
 
+
+        DynamoDbClient dynamoDbClient = DynamoDbClient.create();
+
+        StudentStatusCollector collector = new StudentStatusCollector(
+                dynamoDbClient,
+                tableName,
+                System.getenv("USERS_TABLE"),
+                System.getenv("SESSIONS_TABLE")
+        );
+
+
         // 의존성 수동 주입
         SocketRepository repository = new SocketRepository(
                 DynamoDbConfig.connectDynamoDb(),
                 tableName
         );
-        SocketService service = new SocketService(repository);
+        SocketService service = new SocketService(repository,collector);
         this.controller = new SocketController(service, new Gson());
     }
 
